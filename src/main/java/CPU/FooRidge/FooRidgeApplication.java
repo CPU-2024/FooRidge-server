9a4302bae7f3aa4a6b9c4319f802d87d8d1eae7a
dinmoy <imhyojin614@gmail.com>
2024-02-12 22:40:57 +0900

    spring 프로젝트 생성

---
+++ src/main/java/CPU/FooRidge/FooRidgeApplication.java
@@ -0,0 +1,13 @@
+package CPU.FooRidge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooRidgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FooRidgeApplication.class, args);
+	}
+
+}
