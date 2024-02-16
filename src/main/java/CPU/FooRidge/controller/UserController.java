e0f4fac1d6e0ef1a5e0c27af4d22ad417664e4ef
dinmoy <imhyojin614@gmail.com>
2024-02-16 18:33:17 +0900

    Feat : User Controller 사용자 조회 & 사용자 생성 추가

---
+++ src/main/java/CPU/FooRidge/controller/UserController.java
@@ -0,0 +1,35 @@
+package CPU.FooRidge.controller;
+
+import CPU.FooRidge.domain.User;
+import CPU.FooRidge.repository.UserRepository;
+import CPU.FooRidge.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    //read
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.findAllUser();
+    }
+
+    @PostMapping
+    public ResponseEntity<User> addUser(@RequestBody User user){
+        User createUser = userService.addUser(user);
+        return new ResponseEntity<>(createUser, HttpStatus.CREATED);
+    }
+}
