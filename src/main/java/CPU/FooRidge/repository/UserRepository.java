58bd3fffa695f41f28f2d1a20103f3a2d810cd92
dinmoy <imhyojin614@gmail.com>
2024-02-18 16:46:51 +0900

    Feat : 이메일 검색 메서드 추가

--- src/main/java/CPU/FooRidge/repository/UserRepository.java
+++ src/main/java/CPU/FooRidge/repository/UserRepository.java
@@ -4,4 +4,5 @@ import CPU.FooRidge.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+    User findByUserEmail(String userEmail);
 }
