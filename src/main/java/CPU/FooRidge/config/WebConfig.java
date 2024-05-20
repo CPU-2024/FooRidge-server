ee38ba75e73b0afc92bd32163b97101cb673faa0
dinmoy <imhyojin614@gmail.com>
2024-05-20 23:50:49 +0900

    Refactor : Cors이슈 Configuration 추가

---
+++ src/main/java/CPU/FooRidge/config/WebConfig.java
@@ -0,0 +1,20 @@
+package CPU.FooRidge.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS" , "PATCH")
+                .allowCredentials(true);
+    }
+}
+
