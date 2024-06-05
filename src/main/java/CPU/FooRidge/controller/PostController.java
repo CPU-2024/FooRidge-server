2b2596350b0751963c6b393983ca53a79d4c7b76
dinmoy <imhyojin614@gmail.com>
2024-06-05 22:10:10 +0900

    Comment: 게시물 컨트롤러에 주석 추가

--- src/main/java/CPU/FooRidge/controller/PostController.java
+++ src/main/java/CPU/FooRidge/controller/PostController.java
@@ -22,11 +22,13 @@ public class PostController {
 
     private final PostService postService;
 
+    //게시물 전체 조회
     @GetMapping
     public List<Post> getAllPost() {
         return postService.getAllPost();
     }
 
+    //사용자별 게시물 조회
     @GetMapping("/user/{userId}")
     public ResponseEntity<List<Post>> getPostsByUserId(@PathVariable("userId") Long userId){
         List<Post> posts=postService.getPostsByUserId(userId);
@@ -35,17 +37,19 @@ public class PostController {
                 ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
+    //게시물 검색
     @GetMapping("/search")
     public ResponseEntity<List<Post>> search(@RequestParam("keyword") String keyword){
         List<Post> searchList=postService.search(keyword);
         return ResponseEntity.status(HttpStatus.OK).body((searchList));
     }
 
+    //게시물 생성
     @PostMapping
     public ResponseEntity<Post>  addPost(AddPostRequest dto,
-                                           @RequestParam("file") List<MultipartFile> files){
+                                         @RequestParam("file") List<MultipartFile> files){
         try{
-           Post post=postService.addPost(dto, files);
+            Post post=postService.addPost(dto, files);
             return ResponseEntity.ok(post);
         } catch (IOException e) {
             e.printStackTrace();
@@ -54,13 +58,14 @@ public class PostController {
 
     }
 
-
+    //게시물 삭제
     @DeleteMapping("/{postId}")
     public ResponseEntity<Void> deletePost(@PathVariable("postId") Long postId){
         postService.deletePost(postId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
+    //게시물 수정
     @PutMapping("/{postId}")
     public ResponseEntity<Post> updatePost(@PathVariable("postId") Long postId, UpdatePostRequest dto){
         postService.updatePost(postId,dto);
